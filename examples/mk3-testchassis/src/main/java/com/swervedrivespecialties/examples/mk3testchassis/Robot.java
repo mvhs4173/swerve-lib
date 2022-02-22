1f03c49f71ce91dd04f39980be639b7480d667e6
Colin Wong <cwongmath@gmail.com>
2022-02-22 04:17:45 -0600

    Suppress warnings

--- examples/mk3-testchassis/src/main/java/com/swervedrivespecialties/examples/mk3testchassis/Robot.java
+++ examples/mk3-testchassis/src/main/java/com/swervedrivespecialties/examples/mk3testchassis/Robot.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
+    @SuppressWarnings("unused")
     private final RobotContainer container = new RobotContainer();
 
     @Override
