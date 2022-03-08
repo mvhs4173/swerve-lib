52630a26cab3f303a1fe33166d48d594b67ac484
Colin Wong <cwongmath@gmail.com>
2022-03-08 01:19:07 -0600

    Retry seeding if abs pos fails

--- src/main/java/com/swervedrivespecialties/swervelib/ctre/CanCoderFactoryBuilder.java
+++ src/main/java/com/swervedrivespecialties/swervelib/ctre/CanCoderFactoryBuilder.java
@@ -1,5 +1,6 @@
 package com.swervedrivespecialties.swervelib.ctre;
 
+import com.ctre.phoenix.ErrorCode;
 import com.ctre.phoenix.sensors.AbsoluteSensorRange;
 import com.ctre.phoenix.sensors.CANCoder;
 import com.ctre.phoenix.sensors.CANCoderConfiguration;
@@ -39,6 +40,8 @@ public class CanCoderFactoryBuilder {
     }
 
     private static class EncoderImplementation implements AbsoluteEncoder {
+        private final int ATTEMPTS = 3; // TODO: Allow changing number of tries for getting correct position
+
         private final CANCoder encoder;
 
         private EncoderImplementation(CANCoder encoder) {
@@ -49,7 +52,15 @@ public class CanCoderFactoryBuilder {
         public double getAbsoluteAngle() {
             double angle = Math.toRadians(encoder.getAbsolutePosition());
 
-            CtreUtils.checkCtreError(encoder.getLastError(), "Failed to retrieve CANcoder "+encoder.getDeviceID()+" absolute position");
+            ErrorCode code = encoder.getLastError();
+
+            for (int i = 0; i < ATTEMPTS; i++) {
+                if (code == ErrorCode.OK) break;
+                angle = Math.toRadians(encoder.getAbsolutePosition());
+                code = encoder.getLastError();
+            }
+
+            CtreUtils.checkCtreError(code, "Failed to retrieve CANcoder "+encoder.getDeviceID()+" absolute position after "+ATTEMPTS+" tries");
 
             angle %= 2.0 * Math.PI;
             if (angle < 0.0) {
