d4d8a3b0d92b0b2db516937c10c4b1166743a811
R15 <mvhs4173@gmail.com>
2023-02-14 17:47:17 -0800

    James - removed unused imports and changed climber can id

--- examples/mk3-testchassis/src/main/java/com/swervedrivespecialties/examples/mk3testchassis/commands/SetArmLength.java
+++ examples/mk3-testchassis/src/main/java/com/swervedrivespecialties/examples/mk3testchassis/commands/SetArmLength.java
@@ -5,16 +5,26 @@
 package com.swervedrivespecialties.examples.mk3testchassis.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import com.swervedrivespecialties.examples.mk3testchassis.subsystems.ArmSubsystem;
 
-public class MoveMotor extends CommandBase {
-  /** Creates a new MoveMotor. */
-  public MoveMotor() {
+public class SetArmLength extends CommandBase {
+  private final ArmSubsystem m_arm;
+  private final double m_distance;
+  private static final double m_ArmTolerance = 1;
+  private int m_counter = 0;
+  /** Creates a new SetArmLength. */
+  public SetArmLength(ArmSubsystem arm, double distance) {
+    m_arm = arm;
+    m_distance = distance;
+    addRequirements(m_arm);
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    m_arm.goToPositionInches(m_distance);
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
@@ -27,6 +37,13 @@ public class MoveMotor extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+      boolean isClose = (Math.abs(m_arm.getPositionInches() - m_distance) < m_ArmTolerance);
+      if (isClose) {
+        m_counter++;
+      }
+      else {
+        m_counter = 0;
+      }
+    return (m_counter >= 5);
   }
 }
